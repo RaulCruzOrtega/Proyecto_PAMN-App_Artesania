fda10dbf1c3a9e9b98e6693d6ee7dcc6ec6bc5c1
RaulCruzOrtega <raul_cruz_ortega@outlook.es>
2023-11-14 15:14:42 +0000

    Creación del Proyecto

---
+++ app/src/main/java/com/example/app_artesania/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.app_artesania
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.app_artesania.ui.theme.App_ArtesaniaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App_ArtesaniaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("App Artesania")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "$name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    App_ArtesaniaTheme {
+        Greeting("Android")
+    }
+}
